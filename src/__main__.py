5f5222b4c1624c48863e748b2a3899da7ece9643
Bi2Nb9O3 <92721357+Bi2Nb9O3-Studio@users.noreply.github.com>
2025-01-31 02:32:28 +0800

    bugfix

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,11 @@
 import app.models.database as db
+db.db.init()
 import os
 os.makedirs("./configs/", exist_ok=True)
 os.makedirs("./logs/", exist_ok=True)
 os.makedirs("./models/", exist_ok=True)
 import sys
 import app.setup
-db.db.init()
 import app.auth
 if __name__ == "__main__":
     debug="--debug" in sys.argv
