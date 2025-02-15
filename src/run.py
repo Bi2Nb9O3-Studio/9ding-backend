1253c6bd41fab3a4c35fb80b55e05c751083925f
Bi2Nb9O3 <92721357+Bi2Nb9O3-Studio@users.noreply.github.com>
2025-02-15 19:13:17 +0800

    调整代码结构

--- src/run.py
+++ src/run.py
@@ -1,9 +1,9 @@
 import auto_update as atp
 import os
-os.makedirs("./models/", exist_ok=True)
-atp.download_font()
-atp.update_bundle()
 import app.setup
 
 if __name__ == "__main__":
+    os.makedirs("./models/", exist_ok=True)
+    atp.download_font()
+    atp.update_bundle()
     app.setup.create_app().run(port=5001)
